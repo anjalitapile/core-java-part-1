ec52c50cdad3b2617a53810b1549e71527558356
Anjali Tapile <anjalitapile07@gmail.com>
2023-05-30 08:17:44 -0700

    adding data type example file

---
+++ primitive-non-primitive-data-type/src/DataTypesExample.java
@@ -0,0 +1,27 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        //DataType--> Single Line Comment
+
+        /*
+        Data Types
+           1. Primitive
+                 Type and size
+                 1. byte - 1 byte -> 8 bit -> 2^7 to (2^7-1)
+                 2. short - 2 B -> 16 bit ->2^15 to (2^15-1)
+                 3. char - 2 B -> 16 bit ->2^15 to (2^15-1)
+                 4. int - 4 B -> 32 bit ->2^31 to (2^31-1)
+                 5.float - 4 B-> 32 bit ->2^31 to (2^31-1)
+                 6. Long - 8 B -> 64 bit ->2^63 to (2^63-1)
+                 7. double - 8 B -> 64 bit ->2^63 to (2^63-1)
+                 8. boolean - 1 bit
+
+                 2. Non-Primitive
+                 Class and arrays
+         */
+      boolean bool = true
+      byte singleByte = 10;//byte is data type and singleByte is variable name and 10 is value of var
+
+        System.out.println(singleByte);
+    }
+
+}
